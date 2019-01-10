ae33098d95e4e7ab34375a6843d2f2c6b54fcb5c
Jeff Langr <Jeff@Langrsoft.com>
2019-01-10 15:21:50 -0700

    annotated for code referencing

--- client/src/components/AddSpace.test.js
+++ client/src/components/AddSpace.test.js
@@ -4,9 +4,11 @@ import AddSpace from './AddSpace'
 
 describe('an AddSpace form', () => {
   it('captures the property address', () => {
-    const component = shallow(<AddSpace />)
-    component.find('.input-street-address')
+    const component = shallow(<AddSpace />) //<callout id="code.AddSpace.test.1"/>
+    // START:simulate-change
+    component.find('.input-street-address') //<callout id="code.AddSpace.test.2"/>
       .simulate('change', { target: { value: '1 Main St'}})
-    expect(component.state().streetAddress).toEqual('1 Main St')
+    // END:simulate-change
+    expect(component.state().streetAddress).toEqual('1 Main St') //<callout id="code.AddSpace.test.3"/>
   })
 })

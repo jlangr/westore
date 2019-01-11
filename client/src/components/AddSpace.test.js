994a5848b3b4bc12eae1cb51524e385b516611de
Jeff Langr <Jeff@Langrsoft.com>
2019-01-11 09:42:54 -0700

    extract changeText function

--- client/src/components/AddSpace.test.js
+++ client/src/components/AddSpace.test.js
@@ -2,13 +2,21 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import AddSpace from './AddSpace'
 
+// START:change-text
+//START_HIGHLIGHT
+const changeText = (component, selector, value) =>
+  component.find(selector).simulate('change', { target: { value }})
+//END_HIGHLIGHT
+
 describe('an AddSpace form', () => {
   it('captures the property address', () => {
     const component = shallow(<AddSpace />) //<callout id="code.AddSpace.test.1"/>
-    // START:simulate-change
-    component.find('.input-street-address') //<callout id="code.AddSpace.test.2"/>
-      .simulate('change', { target: { value: '1 Main St'}})
-    // END:simulate-change
+    //START_HIGHLIGHT
+
+    changeText(component, '.input-street-address', '1 Main St')
+
+    //END_HIGHLIGHT
     expect(component.state().streetAddress).toEqual('1 Main St') //<callout id="code.AddSpace.test.3"/>
   })
 })
+// END:change-text

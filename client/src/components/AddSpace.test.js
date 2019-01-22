b75e23442c04fc646189be582099a921b33f7772
Jeff Langr <Jeff@Langrsoft.com>
2019-01-22 13:40:42 -0700

    refactored test

--- client/src/components/AddSpace.test.js
+++ client/src/components/AddSpace.test.js
@@ -5,22 +5,22 @@ import AddSpace from './AddSpace'
 const changeText = (component, selector, value) =>
   component.find(selector).simulate('change', { target: { value }})
 
+// START:refactored-test
 describe('an AddSpace form', () => {
-  it('captures the property address', () => {
-    const component = shallow(<AddSpace />)
+  let component
 
+  beforeEach(() => component = shallow(<AddSpace />))
+
+  it('captures the property address', () => {
     changeText(component, '.input-street-address', '1 Main St')
 
     expect(component.state().streetAddress).toEqual('1 Main St')
   })
 
-  // START:new-test
   it('captures the city', () => {
-    const component = shallow(<AddSpace />)
-
     changeText(component, '.input-city', 'Laurel')
 
     expect(component.state().city).toEqual('Laurel')
   })
-  // END:new-test
 })
+// END:refactored-test

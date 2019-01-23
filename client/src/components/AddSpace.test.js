ac73e706502a6925c748ed937c420cc41256d06c
Jeff Langr <Jeff@Langrsoft.com>
2019-01-23 09:42:36 -0700

    added highlight

--- client/src/components/AddSpace.test.js
+++ client/src/components/AddSpace.test.js
@@ -7,9 +7,11 @@ const changeText = (component, selector, value) =>
 
 // START:refactored-test
 describe('an AddSpace form', () => {
+  // START_HIGHLIGHT
   let component
 
   beforeEach(() => component = shallow(<AddSpace />))
+  // END_HIGHLIGHT
 
   it('captures the property address', () => {
     changeText(component, '.input-street-address', '1 Main St')

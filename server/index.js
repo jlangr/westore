e5a684123375574961719fae63f5b482be875f82
Jeff Langr <Jeff@Langrsoft.com>
2019-01-28 14:08:56 -0700

    v9 remove comment

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ import express from 'express'
 const app = express()
 
 // START_HIGHLIGHT
-// support CORS requests
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
+const allowCORS = app => {
+// END_HIGHLIGHT
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+  })
+}
+// START_HIGHLIGHT
+allowCORS(app)
 // END_HIGHLIGHT
 
 app.route('/space').post((request, response) => {

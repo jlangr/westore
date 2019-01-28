7a1dae56a8164cf3f4c71a7340ce20d1ae724263
Jeff Langr <Jeff@Langrsoft.com>
2019-01-28 14:04:53 -0700

    v8 support CORS

--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@ import express from 'express'
 
 const app = express()
 
+// START_HIGHLIGHT
+// support CORS requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+// END_HIGHLIGHT
+
 app.route('/space').post((request, response) => {
   response.status(200).json(1)
 })

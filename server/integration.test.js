0b46e74cb8f2d34e3aba783021f0746b2a0c3dc6
Jeff Langr <Jeff@Langrsoft.com>
2019-01-28 21:35:16 -0700

    fix body issue with JSON

--- server/integration.test.js
+++ server/integration.test.js
@@ -3,13 +3,10 @@ const axios = require('axios')
 describe('space', () => {
   it('returns id of generated space', async () => {
     const response = await axios.post('http://localhost:3002/space',
-      {
-        body: { 'city': 'X', 'street-address': 'Y'}
-      })
+      { 'city': 'X', 'street-address': 'Y' })
 
     const json = response.data
 
     expect(json).toEqual(1)
   })
 })
-

b02851e2f8ec1526eecfc42aa8ef659806a6b27a
Jeff Langr <Jeff@Langrsoft.com>
2019-02-06 11:28:16 -0700

    wrap fix

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ const app = express()
 const allowCORS = app => {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.header('Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept')
     next()
   })
 }

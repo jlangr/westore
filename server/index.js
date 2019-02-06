ee5a1e04562797b906be98fb4335af2863a613b7
Jeff Langr <Jeff@Langrsoft.com>
2019-02-06 11:26:55 -0700

    wrap

--- server/index.js
+++ server/index.js
@@ -6,7 +6,8 @@ const app = express()
 // support CORS requests
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
 // END_HIGHLIGHT

5e250f6eb71127ddd5261eba20d2fbd3a846a67d
Jeff Langr <Jeff@Langrsoft.com>
2019-02-07 14:13:12 -0700

    unit tests

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,24 @@
-let space = undefined
+// START_HIGHLIGHT
+let spaces = []
+
+const maxId = () => spaces.length
+// END_HIGHLIGHT
 
 export const configure = app => {
   app.route('/space').post(postSpace)
   app.route('/spaces').get(getSpaces)
 }
 
-const postSpace = (request, response) => {
-  space = request.body
-  space.id = 1
-  response.status(200).json(1)
+export const postSpace = (request, response) => {
+  // START_HIGHLIGHT
+  const space = request.body
+  space.id = maxId() + 1
+  spaces.push(space)
+  response.status(200).json(space.id)
+  // END_HIGHLIGHT
 }
 
-const getSpaces = (request, response) =>
-  response.send([ space ])
+export const getSpaces = (request, response) =>
+// START_HIGHLIGHT
+  response.send(spaces)
+// END_HIGHLIGHT

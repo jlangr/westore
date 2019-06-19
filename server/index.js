55e99c33e612370d67b45c8119284ed33addc57f
Jeff Langr <Jeff@Langrsoft.com>
2019-06-19 09:21:35 -0600

    remove highlights

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 import express from 'express'
-// START_HIGHLIGHT
 import bodyParser from 'body-parser'
-// END_HIGHLIGHT
 
 const app = express()
 
@@ -15,23 +13,17 @@ const allowCORS = app => {
 }
 allowCORS(app)
 
-// START_HIGHLIGHT
 app.use(bodyParser.json())
 
 let space = undefined
-// END_HIGHLIGHT
 
 app.route('/space').post((request, response) => {
-// START_HIGHLIGHT
   space = request.body
   space.id = 1
-// END_HIGHLIGHT
   response.status(200).json(1)
 })
 
-// START_HIGHLIGHT
 app.route('/spaces').get((request, response) =>
   response.send([ space ]))
-// END_HIGHLIGHT
 
 app.listen(3002, () => console.log(`server listening`))

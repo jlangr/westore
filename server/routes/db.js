68f48f14cca3d7269754dc20655b7635970b838f
Jeff Langr <Jeff@Langrsoft.com>
2019-06-26 15:28:42 -0600

    factored db

--- server/routes/db.js
+++ server/routes/db.js
@@ -2,22 +2,20 @@ import { MongoClient } from 'mongodb'
 
 const url = 'mongodb://localhost:27017/westore'
 
-export const add = (space) =>
+const inMongoDbContext = onThen =>
   new Promise((resolve, reject) =>
     MongoClient.connect(url)
       .then(client => {
         const db = client.db()
-        db.collection('spaces').insertOne(space)
-          .then(result => resolve(result.ops[0]._id))
+        onThen(resolve, reject, db)
       })
       .catch(error => reject(error)))
+
+export const add = (space) =>
+  inMongoDbContext((resolve, reject, db) =>
+    db.collection('spaces').insertOne(space)
+      .then(result => resolve(result.ops[0]._id)))
 
 export const findAll = () =>
-  new Promise((resolve, reject) =>
-    MongoClient.connect(url)
-      .then(client => {
-        const db = client.db()
-        resolve(db.collection('spaces').find({}).toArray())
-      })
-      .catch(error => reject(error)))
-
+  inMongoDbContext((resolve, reject, db) =>
+    resolve(db.collection('spaces').find({}).toArray()))
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,15 +5,17 @@ export const configure = app => {
   app.route('/spaces').get(getSpaces)
 }
 
+const defaultErrorHandler = error => response.status(500).send(`server error: ${error.message}`)
+
 export const postSpace = (request, response) => {
   const space = request.body
   DB.add(space)
     .then(id => response.status(201).send(id))
-    .catch(error => response.status(500).send(`server error: ${error.message}`))
+    .catch(defaultErrorHandler)
 }
 
 export const getSpaces = (request, response) =>
   DB.findAll()
     .then(spaces => response.json(spaces))
-    .catch(error => response.status(500).send(`server error: ${error.message}`))
+    .catch(defaultErrorHandler)
 

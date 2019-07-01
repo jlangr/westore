b1f0832fe3e314d3a25927b301c41694d5be76cf
Jeff Langr <Jeff@Langrsoft.com>
2019-07-01 14:07:16 -0600

    init c5

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,15 @@
-// START:leaky
 let spaces = []
 
 const maxId = () => spaces.length
 
 const clearAllSpaces = () => spaces.length = 0
 
-// START_HIGHLIGHT
 const addSpace = space => {
   const id = maxId() + 1
   spaces.push(Object.assign({ id }, space))
   return id
 }
-// END_HIGHLIGHT
 
-// ...
-// END:leaky
 export const configure = app => {
   app.route('/space').post(postSpace)
   app.route('/spaces').get(getSpaces)
@@ -22,15 +17,11 @@ export const configure = app => {
     app.route('/spaces/clear').get(clearSpaces)
 }
 
-// START:leaky
 export const postSpace = (request, response) => {
   const space = request.body
-// START_HIGHLIGHT
   const id = addSpace(space)
-// END_HIGHLIGHT
   response.status(200).json(id)
 }
-// END:leaky
 
 export const getSpaces = (request, response) =>
   response.send(spaces)
--- server/routes/index.test.js
+++ server/routes/index.test.js
@@ -1,20 +1,15 @@
 import * as Routes from './index.js'
 import MockExpress from 'jest-mock-express'
 
-// START:clear
 describe('when posting spaces', () => {
   let response
   const aSpace = { city: 'A', address: '1' }
 
   beforeEach(() => {
     response = MockExpress.response()
-// START_HIGHLIGHT
     Routes.clearSpaces({}, MockExpress.response())
-// END_HIGHLIGHT
     Routes.postSpace({ body: aSpace }, response)
   })
-// ...
-// END:clear
 
   it('returns generated ID', () =>
     expect(response.json).toHaveBeenCalledWith(1))
@@ -44,6 +39,4 @@ describe('when posting spaces', () => {
           expect.objectContaining(anotherSpace) ])
     })
   })
-// START:clear
 })
-// END:clear

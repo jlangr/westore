1c6b077642944a57a2c2317b5acf7a7eb6345471
Jeff Langr <Jeff@Langrsoft.com>
2019-07-01 22:48:48 -0600

    refactor pass 1

--- server/routes/db.js
+++ server/routes/db.js
@@ -1,20 +1,18 @@
-import { MongoClient } from 'mongodb'
+import {MongoClient} from 'mongodb'
 
 const url = 'mongodb://localhost:27017/westore'
 
-export const add = async space => {
-  const client = await MongoClient.connect(url, {useNewUrlParser: true})
-  return await client.db().collection('spaces').insertOne(space)
-}
+export const add = async space =>
+  await (await spaces()).insertOne(space)
 
-export const findAll = async () => {
-  const client = await MongoClient.connect(url, {useNewUrlParser: true})
-  return await client.db().collection('spaces').find({}).toArray()
-}
+export const findAll = async () =>
+  await (await spaces()).find({}).toArray()
 
-export const clearAll = async () => {
-  const client = await MongoClient.connect(url, {useNewUrlParser: true})
-  return await client.db().collection('spaces').deleteMany({})
-}
+export const clearAll = async () =>
+  await (await spaces()).deleteMany({})
 
+const spaces = async () => {
+  const client = await MongoClient.connect(url, {useNewUrlParser: true})
+  return client.db().collection('spaces')
+}
 

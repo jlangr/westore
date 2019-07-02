e944eb10b034f6d458fe80d0393c8fdbb41ece03
Jeff Langr <Jeff@Langrsoft.com>
2019-07-01 21:53:23 -0600

    simplified

--- server/routes/db.js
+++ server/routes/db.js
@@ -2,23 +2,19 @@ import { MongoClient } from 'mongodb'
 
 const url = 'mongodb://localhost:27017/westore'
 
-export const add = space =>
-  new Promise(resolve =>
-    MongoClient.connect(url, {useNewUrlParser: true})
-      .then(client => {
-        const db = client.db()
-        // START_HIGHLIGHT
-        resolve(db.collection('spaces').insertOne(space))
-        // END_HIGHLIGHT
-      }))
+export const add = async space => {
+  const client = await MongoClient.connect(url, {useNewUrlParser: true})
+  return await client.db().collection('spaces').insertOne(space)
+}
 
-export const findAll = () =>
-  new Promise(resolve =>
-    MongoClient.connect(url, {useNewUrlParser: true})
-      .then(client => {
-        const db = client.db()
-        // START_HIGHLIGHT
-        resolve(db.collection('spaces').find({}).toArray())
-        // END_HIGHLIGHT
-      }))
+export const findAll = async () => {
+  const client = await MongoClient.connect(url, {useNewUrlParser: true})
+  return await client.db().collection('spaces').find({}).toArray()
+}
+
+export const clearAll = async () => {
+  const client = await MongoClient.connect(url, {useNewUrlParser: true})
+  return await client.db().collection('spaces').deleteMany({})
+}
+
 
--- server/routes/db.test.js
+++ server/routes/db.test.js
@@ -1,6 +1,8 @@
 import * as DB from './db'
 
 describe('a space database', () => {
+  beforeEach(() => DB.clearAll())
+
   it('retrieves persisted space', async () => {
     await DB.add({ city: 'C', address: '99' })
 

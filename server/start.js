38d9aa273b85eaf7be186036373d378d6a9637c9
Jeff Langr <Jeff@Langrsoft.com>
2019-07-31 22:06:05 -0500

    test mode script and console log

--- server/start.js
+++ server/start.js
@@ -4,4 +4,9 @@ require('babel-register')({ presets: ['env']})
 global.isTestServer = () => process.argv[2] === '--test'
 // END_HIGHLIGHT
 
+if (global.isTestServer())
+  console.log('***test mode*** test functions enabled')
+else
+  console.log('prod mode; test functions disabled')
+
 module.exports = require('./index.js')

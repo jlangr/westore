a10ce7577637fe0759c541ff9f4b1876329ecbf2
Jeff Langr <Jeff@Langrsoft.com>
2019-08-07 21:00:28 -0400

    dbv3

--- server/persistence/db.js
+++ server/persistence/db.js
@@ -2,29 +2,22 @@ import { MongoClient } from 'mongodb'
 
 const url = 'mongodb://localhost:27017/westore'
 
-export const add = space =>
-  new Promise((resolve, _reject) =>
-    MongoClient.connect(url, { useNewUrlParser: true })
-      .then(client => {
-        const db = client.db()
-        resolve(db.collection('spaces').insertOne(space))
-      }))
+export const add = (space) => inMongoDbContext((resolve, reject, db) =>
+  resolve(spaces(db).insertOne(space)))
 
-export const findAll = () =>
-  new Promise((resolve, _reject) =>
-    MongoClient.connect(url, { useNewUrlParser: true })
-      .then(client => {
-        const db = client.db()
-        resolve(db.collection('spaces').find({}).toArray())
-      }))
+export const findAll = () => inMongoDbContext((resolve, reject, db) =>
+  resolve(spaces(db).find({}).toArray()))
 
-export const clearAll = () =>
-  new Promise((resolve, _reject) =>
+export const clearAll = () => inMongoDbContext((resolve, reject, db) =>
+  resolve(spaces(db).deleteMany({})))
+
+const inMongoDbContext = onThen =>
+  new Promise((resolve, reject) =>
     MongoClient.connect(url, { useNewUrlParser: true })
-      .then(client => {
-        const db = client.db()
-        resolve(db.collection('spaces').deleteMany({}))
-      }))
+      .then(client => onThen(resolve, reject, client.db())))
+
+const spaces = db => db.collection('spaces')
+
 
 
 
